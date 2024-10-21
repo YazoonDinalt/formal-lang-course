ee53557d81bcc81b2fb9f5f2045f4d720aa01ab3
YazoonDinalt <kykyky228kykyk@gmail.com>
2024-10-21 21:23:00 +0300

    refactor: Change format
    Signed-off-by: YazoonDinalt <kykyky228kykyk@gmail.com>

--- project/task4.py
+++ project/task4.py
@@ -27,25 +27,30 @@ def get_from_block(M: csr_matrix, n: int, m: int, k: int, i: int, j: int) -> boo
 
 
 def update_front(
-  front: csr_matrix,
-  dfa_boolean_decomposition: dict[Symbol, csr_array],
-  nfa_boolean_decomposition: dict[Symbol, csr_array],
-  k: int,
-  n: int,
-  symbols: set[Symbol],
+    front: csr_matrix,
+    dfa_boolean_decomposition: dict[Symbol, csr_array],
+    nfa_boolean_decomposition: dict[Symbol, csr_array],
+    k: int,
+    n: int,
+    symbols: set[Symbol],
 ) -> csr_matrix:
-  decomposed_fronts = {symbol: front @ nfa_boolean_decomposition[symbol] for symbol in symbols}
+    decomposed_fronts = {
+        symbol: front @ nfa_boolean_decomposition[symbol] for symbol in symbols
+    }
 
-  for symbol, decomposed_front in decomposed_fronts.items():
-    for i in range(k):
-      start_idx, end_idx = n * i, n * (i + 1)
-      decomposed_front[start_idx:end_idx] = dfa_boolean_decomposition[symbol].T @ decomposed_front[start_idx:end_idx]
+    for symbol, decomposed_front in decomposed_fronts.items():
+        for i in range(k):
+            start_idx, end_idx = n * i, n * (i + 1)
+            decomposed_front[start_idx:end_idx] = (
+                dfa_boolean_decomposition[symbol].T
+                @ decomposed_front[start_idx:end_idx]
+            )
 
-  result = reduce(
-    lambda acc, val: acc + val, decomposed_fronts.values(), csr_matrix(front.shape)
-  )
+    result = reduce(
+        lambda acc, val: acc + val, decomposed_fronts.values(), csr_matrix(front.shape)
+    )
 
-  return result
+    return result
 
 
 def ms_bfs_based_rpq(
@@ -63,9 +68,14 @@ def ms_bfs_based_rpq(
     nfa_index_to_state_map = {idx: state for state, idx in nfa_matrix.states.items()}
     start_state_pairs = product(dfa_matrix.start_states, nfa_matrix.start_states)
     pair_count = len(dfa_matrix.start_states) * len(nfa_matrix.start_states)
-    common_alphabet = dfa_matrix.boolean_decomposition.keys() & nfa_matrix.boolean_decomposition.keys()
+    common_alphabet = (
+        dfa_matrix.boolean_decomposition.keys()
+        & nfa_matrix.boolean_decomposition.keys()
+    )
 
-    current_front = create_initial_front(dfa_state_count, nfa_state_count, start_state_pairs)
+    current_front = create_initial_front(
+        dfa_state_count, nfa_state_count, start_state_pairs
+    )
     explored_states = current_front.copy()
     while current_front.nnz != 0:
         next_front = update_front(
@@ -83,7 +93,9 @@ def ms_bfs_based_rpq(
     final_result = set()
     for dfa_final_state in dfa_matrix.final_states:
         for index, nfa_start in enumerate(nfa_matrix.start_states):
-            state_block = explored_states[dfa_state_count * index : dfa_state_count * (index + 1)]
+            state_block = explored_states[
+                dfa_state_count * index : dfa_state_count * (index + 1)
+            ]
             for reachable_state in state_block.getrow(dfa_final_state).indices:
                 if reachable_state in nfa_matrix.final_states:
                     final_result.add(
