5b2d6ed1e5cce15c5ea2c91fdd39cfda7ff09f54
YazoonDinalt <kykyky228kykyk@gmail.com>
2024-10-22 02:19:55 +0300

    feat: Small refactor
    Signed-off-by: YazoonDinalt <kykyky228kykyk@gmail.com>

--- project/task4.py
+++ project/task4.py
@@ -1,103 +1,81 @@
 from functools import reduce
-from itertools import product
-import networkx as nx
-from pyformlang.finite_automaton import Symbol
+
 import numpy as np
-from numpy import bool_
-from scipy.sparse import csr_array, csr_matrix
+from networkx import MultiDiGraph
+from pyformlang.finite_automaton import Symbol
+from scipy.sparse import csr_matrix
 
 from project.automata import graph_to_nfa, regex_to_dfa
 from project.task3 import AdjacencyMatrixFA
-from numpy.typing import NDArray
 
 
-def create_array(n: int, m: int, i: int, j: int) -> NDArray[bool_]:
-    a = np.zeros((n, m), dtype=bool_)
-    a[i, j] = True
-    return a
+def start_front(dfa: AdjacencyMatrixFA, nfa: AdjacencyMatrixFA):
+    dfa_start_state = list(dfa.start_states)[0]
+    data = np.ones(len(nfa.start_states), dtype=bool)
+    rows = [
+        dfa_start_state + dfa.states_count * i for i in range(len(nfa.start_states))
+    ]
+    cols = list(nfa.start_states)
 
-
-def create_initial_front(n: int, m: int, start_states: set[int]) -> csr_matrix:
-    arrays = [[create_array(n, m, i, j)] for (i, j) in start_states]
-    return csr_matrix(np.block(arrays))
-
-
-def get_from_block(M: csr_matrix, n: int, m: int, k: int, i: int, j: int) -> bool_:
-    return M[n * k + i, j]
-
-
-def update_front(
-    front: csr_matrix,
-    dfa_boolean_decomposition: dict[Symbol, csr_array],
-    nfa_boolean_decomposition: dict[Symbol, csr_array],
-    k: int,
-    n: int,
-    symbols: set[Symbol],
-) -> csr_matrix:
-    decomposed_fronts = {}
-    for sym in symbols:
-        decomposed_fronts[sym] = front @ nfa_boolean_decomposition[sym]
-
-        for i in range(k):
-            decomposed_fronts[sym][n * i : n * (i + 1)] = (
-                dfa_boolean_decomposition[sym].T
-                @ decomposed_fronts[sym][n * i : n * (i + 1)]
-            )
-    return reduce(
-        lambda x, y: x + y, decomposed_fronts.values(), csr_matrix(front.shape)
+    return csr_matrix(
+        (data, (rows, cols)),
+        shape=(dfa.states_count * len(nfa.start_states), nfa.states_count),
+        dtype=bool,
     )
 
 
 def ms_bfs_based_rpq(
-    regex: str,
-    graph: nx.MultiDiGraph,
-    start_nodes: set[int],
-    final_nodes: set[int],
+    regex: str, graph: MultiDiGraph, start_nodes: set[int], final_nodes: set[int]
 ) -> set[tuple[int, int]]:
-    dfa_from_regex = regex_to_dfa(regex)
-    nfa_from_graph = graph_to_nfa(graph, start_nodes, final_nodes)
-    dfa_matrix = AdjacencyMatrixFA(dfa_from_regex)
-    nfa_matrix = AdjacencyMatrixFA(nfa_from_graph)
-    dfa_state_count = len(dfa_matrix.states)
-    nfa_state_count = len(nfa_matrix.states)
-    nfa_index_to_state_map = {idx: state for state, idx in nfa_matrix.states.items()}
-    start_state_pairs = product(dfa_matrix.start_states, nfa_matrix.start_states)
-    pair_count = len(dfa_matrix.start_states) * len(nfa_matrix.start_states)
-    common_alphabet = (
-        dfa_matrix.boolean_decomposition.keys()
-        & nfa_matrix.boolean_decomposition.keys()
+    regex_dfa = regex_to_dfa(regex)
+    graph_nfa = graph_to_nfa(
+        graph=graph, start_states=start_nodes, final_states=final_nodes
     )
 
-    current_front = create_initial_front(
-        dfa_state_count, nfa_state_count, start_state_pairs
-    )
-    explored_states = current_front.copy()
-    while current_front.nnz != 0:
-        next_front = update_front(
-            current_front,
-            dfa_matrix.boolean_decomposition,
-            nfa_matrix.boolean_decomposition,
-            pair_count,
-            dfa_state_count,
-            common_alphabet,
-        )
-        unvisited_states = next_front > explored_states
-        explored_states += unvisited_states
-        current_front = unvisited_states
+    regex_amfa = AdjacencyMatrixFA(regex_dfa)
+    graph_amfa = AdjacencyMatrixFA(graph_nfa)
 
-    final_result = set()
-    for dfa_final_state in dfa_matrix.final_states:
-        for index, nfa_start in enumerate(nfa_matrix.start_states):
-            state_block = explored_states[
-                dfa_state_count * index : dfa_state_count * (index + 1)
-            ]
-            for reachable_state in state_block.getrow(dfa_final_state).indices:
-                if reachable_state in nfa_matrix.final_states:
-                    final_result.add(
+    init_front = start_front(regex_amfa, graph_amfa)
+    result = set()
+    reg_amfa_transpose_matrices = {
+        symbol: matrix.transpose() for symbol, matrix in regex_amfa.matrix.items()
+    }
+    visited = init_front
+    symbols = [
+        sym for sym in regex_amfa.matrix.keys() if sym in graph_amfa.matrix.keys()
+    ]
+
+    while init_front.toarray().any():
+        next_fronts: dict[Symbol, csr_matrix] = {}
+
+        for symbol in symbols:
+            next_fronts[symbol] = init_front @ graph_amfa.matrix[symbol]
+
+            for i in range(len(graph_amfa.start_states)):
+                dfa_states_cnt = regex_amfa.states_count
+                start_ind, end_ind = i * dfa_states_cnt, (i + 1) * dfa_states_cnt
+                next_fronts[symbol][start_ind:end_ind] = (
+                    reg_amfa_transpose_matrices[symbol]
+                    @ next_fronts[symbol][start_ind:end_ind]
+                )
+
+        init_front = reduce(lambda x, y: x + y, next_fronts.values(), init_front)
+        init_front = init_front > visited
+        visited += init_front
+
+    reversed_nfa_states = {value: key for key, value in graph_amfa.states.items()}
+
+    for dfa_fn_state in regex_amfa.final_states:
+        for i, nfa_start_state in enumerate(graph_amfa.start_states):
+            for nfa_reached in visited.getrow(
+                regex_amfa.states_count * i + dfa_fn_state
+            ).indices:
+                if nfa_reached in graph_amfa.final_states:
+                    result.add(
                         (
-                            nfa_index_to_state_map[nfa_start],
-                            nfa_index_to_state_map[reachable_state],
+                            reversed_nfa_states[nfa_start_state],
+                            reversed_nfa_states[nfa_reached],
                         )
                     )
 
-    return final_result
+    return result
